3ec80db08a3a6e720b7bd95c29574b6649779816
Naman Kansal <namankansaldui2000@gmail.com>
2025-01-31 17:35:40 +0530

    update the put api

--- routers/episodes.py
+++ routers/episodes.py
@@ -55,7 +55,7 @@ def update_episode(episode_id: int, episode: EpisodeUpdate):
     print("existing_episode:", existing_episode)
     print("episode update: ", episode.model_dump())
     for key, value in episode.model_dump().items():
-        existing_episode[key] = value
+        if value: existing_episode[key] = value
     query = text("UPDATE episodes SET title = :title, description = :description, duration = :duration, audio_link = :audio_link, is_bookmark = :is_bookmark, is_deleted = :is_deleted WHERE id = :id")
     execute(query, params=existing_episode)
     return existing_episode
