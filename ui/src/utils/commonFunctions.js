a835436ade667e02e54441e8f66e6e9aa5fa3490
Siddharth Verma <siddharth.verman@gmail.com>
2025-01-31 18:40:00 +0530

    add util func

--- ui/src/utils/commonFunctions.js
+++ ui/src/utils/commonFunctions.js
@@ -1,4 +1,16 @@
+import { isToday, isYesterday } from 'date-fns';
 export const parseFloatToFixed = (value) => {
   const formattedValue = parseFloat(value).toFixed(1);
   return formattedValue.endsWith('.0') ? formattedValue.slice(0, -2) : formattedValue;
+};
+
+export const getRelativeDate = (date) => {
+  if (isToday(date)) {
+    return 'Today';
+  }
+  if (isYesterday(date)) {
+    return 'Yesterday';
+  }
+  const distance = formatDistanceToNow(date, { addSuffix: true });
+  return distance;
 };
