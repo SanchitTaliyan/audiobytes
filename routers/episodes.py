973d7fff45764e39d8de2565cbc9f89b4348111d
Naman Kansal <namankansaldui2000@gmail.com>
2025-01-31 17:32:39 +0530

    update the put api

--- routers/episodes.py
+++ routers/episodes.py
@@ -53,8 +53,9 @@ def update_episode(episode_id: int, episode: EpisodeUpdate):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Episode not found")
     existing_episode = existing_episodes[0]
     print("existing_episode:", existing_episode)
+    print("episode update: ", episode.model_dump())
     for key, value in episode.model_dump().items():
-        setattr(existing_episode, key, value)
+        existing_episode[key] = value
     query = text("UPDATE episodes SET title = :title, description = :description, duration = :duration, audio_link = :audio_link, is_bookmark = :is_bookmark, is_deleted = :is_deleted WHERE id = :episode_id")
     execute(query, params=existing_episode)
     return existing_episode
