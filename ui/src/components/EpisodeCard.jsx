2e9ff96cb3c5afc407cbc7962940fa91a4ccfa2b
Siddharth Verma <siddharth.verman@gmail.com>
2025-01-31 20:30:32 +0530

    fix date render

--- ui/src/components/EpisodeCard.jsx
+++ ui/src/components/EpisodeCard.jsx
@@ -1,51 +1,68 @@
-import React from 'react';
-import { getRelativeDate, parseFloatToFixed } from 'utils/commonFunctions';
-import { BookmarkFilledIcon, BookmarkIcon, PlayIcon } from 'icons';
-import { format } from 'date-fns';
-import eod from 'assets/eod.png';
-import mid from 'assets/mid.png';
-import morning from 'assets/morning.png';
-import weekly from 'assets/weekly.png';
-import monthly from 'assets/monthly.png';
+import React from "react";
+import { getRelativeDate, parseFloatToFixed } from "utils/commonFunctions";
+import { BookmarkFilledIcon, BookmarkIcon, PlayIcon } from "icons";
+import { format } from "date-fns";
+import eod from "assets/eod.png";
+import mid from "assets/mid.png";
+import morning from "assets/morning.png";
+import weekly from "assets/weekly.png";
+import monthly from "assets/monthly.png";
 
 const EpisodeCard = ({ episode, toggleBookmark }) => {
-  const { title, description, duration, published_at, audio_link, is_bookmark, time_of_day } = episode;
+  const {
+    title,
+    description,
+    duration,
+    published_at,
+    audio_link,
+    is_bookmark,
+    time_of_day,
+  } = episode;
   let thumbnailSrc;
 
   switch (time_of_day) {
-    case 'MONTHLY':
+    case "MONTHLY":
       thumbnailSrc = monthly;
       break;
-    case 'WEEKLY':
+    case "WEEKLY":
       thumbnailSrc = weekly;
       break;
-    case 'MORNING':
+    case "MORNING":
       thumbnailSrc = morning;
       break;
-    case 'MIDDAY':
+    case "MIDDAY":
       thumbnailSrc = mid;
       break;
-    case 'ENDOFDAY':
+    case "ENDOFDAY":
       thumbnailSrc = eod;
       break;
   }
 
   return (
-    <div className='p-5 bg-black flex gap-2 min-w-96 w-full border-b border-[#3A3A3C]'>
-      <img className='h-11 w-11 rounded-md' src={thumbnailSrc} />
-      <div className='flex flex-col gap-0.25 overflow-hidden grow'>
-        <div className='text-white text-base font-semibold leading-5'>
-          {title} | {format(published_at, 'dd  yyyy')}
+    <div className="flex w-full min-w-96 gap-2 border-b border-[#3A3A3C] bg-black p-5">
+      <img className="h-11 w-11 rounded-md" src={thumbnailSrc} />
+      <div className="gap-0.25 flex grow flex-col overflow-hidden">
+        <div className="text-base font-semibold leading-5 text-white">
+          {title} | {format(published_at, "dd MMM yyyy")}
         </div>
-        <div className='text-gray-300 text-sm text-nowrap font-normal leading-4.5 truncate'>{description}</div>
-        <div className='flex gap-2 pt-3 justify-center items-center'>
-          <div className='text-gray-300 text-xs leading-3 grow'>{getRelativeDate(published_at)}</div>
-          <div className='flex gap-2 px-2.5 py-1.5 bg-[#3A3A3C] rounded-full'>
+        <div className="leading-4.5 truncate text-nowrap text-sm font-normal text-gray-300">
+          {description}
+        </div>
+        <div className="flex items-center justify-center gap-2 pt-3">
+          <div className="grow text-xs leading-3 text-gray-300">
+            {getRelativeDate(published_at)}
+          </div>
+          <div className="flex gap-2 rounded-full bg-[#3A3A3C] px-2.5 py-1.5">
             <PlayIcon height={16} width={12} />
             {/* <ProgressBar/> */}
-            <div className='text-white text-sm leading-4'>{parseFloatToFixed(duration / 60)} m</div>
+            <div className="text-sm leading-4 text-white">
+              {parseFloatToFixed(duration / 60)} m
+            </div>
           </div>
-          <div className='cursor-pointer' onClick={() => toggleBookmark(episode.id, !is_bookmark)}>
+          <div
+            className="cursor-pointer"
+            onClick={() => toggleBookmark(episode.id, !is_bookmark)}
+          >
             {is_bookmark ? <BookmarkFilledIcon /> : <BookmarkIcon />}
           </div>
         </div>
